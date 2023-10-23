bff29da47493fcf10f4cafc7203d7fb8c5cfc2a2
Miho Naozumi <naozumi.miho@emc.com>
2023-10-23 15:59:27 +0900

    Add files via upload
    1st sample code

---
+++ app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+### This is a simple flask hello world application ###
+# Import modules required for app
+import os
+from flask import Flask, render_template, request
+##### Create a Flask instance
+app = Flask(__name__)
+##### Define routes #####
+@app.route('/')
+def home(): 
+    return 'Hello, World!'
+    #Using template
+    #return render_template('default.html',url="home")
+##### Run the Flask instance, browse to http://<< Host IP or URL >>:5000 #####
+if __name__ == "__main__":
+	app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', '5000')), threaded=True)
