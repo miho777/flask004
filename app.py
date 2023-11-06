63173bebc479402fe22ab3ce287d8a739bffcd68
miho777 <naozumi.miho@emc.com>
2023-11-06 21:41:24 +0900

    Get OS env AAA

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ app = Flask(__name__)
 ##### Define routes #####
 @app.route('/')
 def home(): 
-    #return 'Hello, World!'
-    #Using template
-    return render_template('default.html',url="home")
+    RET_TEXT = 'Hello, World! ' + os.getenv('AAA','DefaultAAA')
+    return RET_TEXT
+    #return 'Hello, World!' 
+    ### Using template
+    #return render_template('default.html',url="home")
 ##### Run the Flask instance, browse to http://<< Host IP or URL >>:5000 #####
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
